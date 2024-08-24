cb303907464e4f7ea2960bb67b0c56dfc6e9a320
Sameetonur <130676651+Sameetonur@users.noreply.github.com>
2024-08-24 14:58:34 +0300

    24.08.2024

---
+++ 04 c# Programlama 24.08.2024/24.08.2024/Proje01_VaribalSamples/Program.cs
@@ -0,0 +1,93 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Proje01_VaribalSamples;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        #region YaşHesaplama
+
+        // kullanıcıdan doğum yılını alıp yaşını hesaplaycak ve bunu ekrana yazdıracak kodu hazırlıyoruz.
+
+        // Console.Write("Doğum yılınzı giriniz : ");
+        // string Tarih = Console.ReadLine();
+        // int dogTarih = Convert.ToInt32(Tarih);
+        // int age = DateTime.Now.Year - dogTarih;
+        // string message = "Yaşınız: " + age;
+        // System.Console.WriteLine(age);
+
+        #endregion
+
+        #region Ürün Fiyatı Hesaplama
+
+        // bir ürünün fiyatı girildiğinde onun kdv dahil fiyatını hesaplayp hem ham hemde kdv dahil fiyatını ekrana yazdıracağız.
+        // System.Console.Write("Girilen Ürün Fiyatı :");
+
+        // decimal ÜrünFiyat = decimal.Parse(Console.ReadLine());
+
+        // double vatRate = 0.18;
+        // double vat = (double)ÜrünFiyat * vatRate;
+        // decimal total = ÜrünFiyat + (decimal)vat;
+
+        // string resulMessage = $@"
+        //  Ürün Fiyatı     : {ÜrünFiyat}
+        //  KDV             : {vat}
+        //  KDV Dahil Fiyat : {total} 
+        //     ";
+        // System.Console.WriteLine(resulMessage);
+        //   string resulMessage = $" Ürün Fiyatı  : {ÜrünFiyat}\n KDV : {vat}\n KDV Dahil Fiyat : {total}\n ";
+        //   System.Console.WriteLine(resulMessage);
+
+
+
+
+
+
+        #endregion
+
+        #region Sıcaklık Dönüştürme
+
+
+        //Kullancıdan celcisus cinsinden alınan sıcaklık değereini fahrenheit cinsine çevirip  ekrana her ikisini de alt alta yazdıran kodu hazırlayınız.
+        // System.Console.Write("Kullanıcıdan Girilen Sıcaklık : ");
+        // decimal girilenSıcaklık = decimal.Parse(System.Console.ReadLine());
+
+        // double çevirme = 33.8;
+        // double çevVeri = (double)girilenSıcaklık * çevirme;
+        // decimal sonHal = girilenSıcaklık + (decimal)çevVeri;
+        // Console.Clear();
+        // string resulMessage = $@"
+        //   Sıcaklık (C)                  : {girilenSıcaklık}
+        //   Fahrenheit (F)                : {çevirme}
+        //   Fahrenheit Çevrilmiş Hali (F) : {sonHal} 
+        //      ";
+        // System.Console.WriteLine(resulMessage);
+
+
+
+
+
+        #endregion
+
+        #region Ağırlık Döönüştürme
+
+        // kullanıcının girdiği kg cinsinden ağırlığı grama çevirip sonucu ekrana yazdıran kodu yazdırınız.
+
+        Console.Write("Ağırlığı giriniz : ");
+        double kgAğırlık = double.Parse(Console.ReadLine());
+
+        double gramCevirme = kgAğırlık * 1000;
+        Console.Clear();
+        string resulMessage = $@"
+           Ağırlık (KG)             : {kgAğırlık}         
+           Gram Çevrilmiş Hali (GR) : {(int)gramCevirme} 
+              ";
+        System.Console.WriteLine("SONU:");
+        System.Console.WriteLine();
+        System.Console.WriteLine(resulMessage);
+
+
+        #endregion
+    }
+}
---
+++ 04 c# Programlama 24.08.2024/24.08.2024/Proje02_StringMethods/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace Proje02StringMethods;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //  string fullName = "halil Umut MELER";
+
+        // int len = fullName.Length;
+
+        // System.Console.WriteLine(len);
+        // Console.WriteLine(fullName.ToLower());//küçük harfe çevirir
+
+        // Console.WriteLine(fullName.ToUpper());// büyük harfe çevirir
+
+        // string address1="Canakkale Şeh. Caddesi";
+        // string address2="No : 16 D:5 K: 5";
+        // string address3="Sultangazi";
+        // string address4="İstanbul";
+
+        //  string address = address1 +" "+ address2 +" "+ address3+ "/" + address4;
+        //  System.Console.WriteLine(address);
+
+        //  string fulladdress = $"{address1} {address2} {address3}/{address4}";
+        //  System.Console.WriteLine(fulladdress);
+
+        // string text1 ="Nişantaşı";
+        // string text2 = "Üniversitesi";
+        // string loc1 = "Bayrampaşa";
+        // string currentLoc = "Sarıyer";
+        // string date1 = " 09.09.2009";
+        // string date2 = "05.05.2015";
+
+        // // Nişantaşı üniversitesi 
+        // string aciklama = $" {text1} {text2}, {date1} tarihinde İstanbul'un {loc1} İlçesinde faaliyete başladı. Ardından {date2} tarihinde yine İstanbul'un {currentLoc} ilçesinde kurulan TechCampus'e taşındı ";
+
+        // System.Console.WriteLine(aciklama);
+
+        // System.Console.WriteLine("----------------------------------");
+        // string aciklama2 = String.Concat(text1," ", text2,", ",date1," tarihinde İstanbul'un", loc1," ilçesinde faaliyete başladı. Ardından ", date2," tarihinde yine İstanbulun", currentLoc, "İlçesinde kurulan TechCampus'e taşındı.");
+
+        // System.Console.WriteLine(aciklama2);
+
+        // string fullName = "halil Umut MELER";
+        // string fineText = "umut";
+        // bool response = fullName.ToLower().Contains(fineText.ToLower()); //constains küçük harf büyük harf  duyarlıdır.(o yüzden ToLower ile metni komple küçük harfe çevirip küçük harf ile sorgulatıyoruz.)
+        // System.Console.WriteLine(response);
+
+        // string firstName = "Begüm";
+        // Console.WriteLine(firstName.IndexOf("ü"));// sarayı bulmak için kullanılır IndexOf.(-1)
+
+        // string firstName = " Ayşen Umay";
+        // System.Console.WriteLine(firstName.StartsWith("Ayşen"));
+        // System.Console.WriteLine(firstName.EndsWith("Umay"));
+
+        string newTitle = "Dolar'ın ateşi çıktı!";
+        string new1 = "54651";
+        string new2 = "Başlık";
+        newTitle = newTitle.ToLower();
+
+
+        string newUrl = newTitle.Replace(" ", "-");
+        newUrl = newUrl.Replace("'", "");
+        newUrl = newUrl.Replace("ş", "s");
+        newUrl = newUrl.Replace("ç", "c");
+        newUrl = newUrl.Replace("!", "");
+        newUrl = newUrl.Replace("ı", "i");
+
+        newUrl = $"{newUrl}-{new1}-{new2}";
+
+        System.Console.WriteLine(newUrl);
+
+
+    }
+}
