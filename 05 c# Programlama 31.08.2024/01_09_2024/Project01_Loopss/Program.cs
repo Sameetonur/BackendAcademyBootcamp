1a250d171266a12675eeafd7e2150e20a2e7842d
Sameetonur <130676651+Sameetonur@users.noreply.github.com>
2024-09-01 19:32:10 +0300

    01.09.2024

--- 05 c# Programlama 31.08.2024/01_09_2024/Project01_Loopss/Program.cs
+++ 05 c# Programlama 31.08.2024/01_09_2024/Project01_Loopss/Program.cs
@@ -26,38 +26,119 @@ class Program
 
         //sadece sayı girişi yaptıralım.
 
-        ConsoleKeyInfo karakter;
-        string resNum = "";
+        // ConsoleKeyInfo karakter;
+        // string resNum = "";
 
-        System.Console.Write("Bir Sayı Giriniz =");
+        // System.Console.Write("Bir Sayı Giriniz =");
 
+        // do
+        // {
+        //     karakter = Console.ReadKey(true);//kontrol etmek için defult(false) olarak girilen veriyi ekrana giriceğimiz veriyi ekrana yazdırır ama biz sayı girmesini istediğimiz için ekrana basmasın(true çekiyoruz(manipüle ediyoruz)) ve sayıysa yazmasını istenilen sorguya götürebiliyoruz ve ona göre yazdıyoruz (yani eksana string veya sayı olmayan veriyi gösterme sadece sayıysa göster diyerek true çekiyoruz)
+        //     if(char.IsDigit(karakter.KeyChar))
+        //     {
+        //         resNum += karakter.KeyChar.ToString();
+        //         Console.Write(karakter.KeyChar.ToString());
+        //     }
+        //     else
+        //     {
+        //         if (karakter.Key ==ConsoleKey.Backspace && resNum.Length > 0)
+        //         {
+        //             resNum = resNum.Substring(0, resNum.Length - 1); // substring (index1,index2) index1=nereye gidiceği,index2= ondan sonra ne kadarı alınacağı
+        //             System.Console.Write("\b \b");
+
+        //         }
+
+        //     }
+
+
+        // } while (karakter.Key != ConsoleKey.Enter);
+
+        /*-----------------------------------------------------------------------------------------------------------*/
+        // bool isPrime = true;
+        // System.Console.WriteLine("Bir sayı giriniz = ");
+        // int num = int.Parse(Console.ReadLine());
+        // if (num <= 1)
+        // {
+        //     isPrime = false;
+        // }
+        // else
+        // {
+        //     for (int i = 2; i < num - 1; i++)
+        //     {
+        //         if (num % i == 0)
+        //         {
+        //             isPrime = false;
+        //             break;
+        //         }
+
+        //     }
+        // }
+        // if (isPrime)
+        // {
+        //     System.Console.WriteLine($"{num}Sayı Asaldır");
+        // }
+        // else
+        // {
+        //     System.Console.WriteLine($"{num}Sayı Asaldeğildir.");
+        // }
+
+        /*-----------------------------------------------------------------------------------------------------------*/
+        // bool kontrol = true;
+        // int num = 0;
+
+        // while (num <= 1)
+        // {
+        //     System.Console.WriteLine("Bir sayı giriniz =");
+        //     num = int.Parse(Console.ReadLine());
+
+        //     for (int i = 2; i < num - 1; i++)
+        //     {
+        //         if(num % 2 == 0)
+        //         {
+        //             kontrol=false;
+        //             break;
+        //         }
+        //     }
+        // }
+        // if (kontrol)
+        // {
+        //     System.Console.WriteLine($"{num} sayısı asal");
+        // }
+        // else
+        // {
+        //     System.Console.WriteLine($"{num} sayısı asaldeğil");
+        // }
+
+        /*-----------------------------------------------------------------------------------------------------------*/
+        bool kontrol = true;
+        int num = 0;
+        string answer;
         do
         {
-            karakter = Console.ReadKey(true);
-            if(char.IsDigit(karakter.KeyChar))
+            System.Console.WriteLine("Bir sayı giriniz =");
+            num = int.Parse(Console.ReadLine());
+
+            for (int i = 2; i < num - 1; i++)
+                if (num % 2 == 0)
+                {
+                    kontrol = false;
+                    break;
+                }
+            if (kontrol)
             {
-                resNum += karakter.KeyChar.ToString();
-                Console.Write(karakter.KeyChar.ToString());
+                System.Console.WriteLine($"{num} sayısı asal");
             }
             else
             {
-                if (karakter.Key ==ConsoleKey.Backspace && resNum.Length > 0)
-                {
-                    resNum = resNum.Substring(0, resNum.Length - 1);
-                    System.Console.Write("\b \b");
-
-                }
-
+                System.Console.WriteLine($"{num} sayısı asaldeğil");
             }
-
-
-        } while (karakter.Key != ConsoleKey.Enter);
-
-        #region Do While
+            System.Console.WriteLine("Tekrar denemek ister misin(e/h)");
+            answer = Console.ReadLine();
 
 
 
-        #endregion
+        } while (answer != "h");
+
 
 
     }
---
+++ 05 c# Programlama 31.08.2024/01_09_2024/Project01_Loopss/Project2_GuessNumberGame/Program.cs
@@ -0,0 +1,96 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Project2_GuessNumberGame;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rnd = new Random();
+
+        int generatedNumber = rnd.Next(1, 101);
+        Console.WriteLine($"{generatedNumber} (Random Sayısı) ");
+
+        Console.WriteLine("------------------------------------------------------------------");
+        System.Console.WriteLine();
+
+        int guessNumber;
+        int live = 1;
+        int liveLimit = 5;
+        string resultMessage = "";
+        string deneme = "";
+
+        do
+        {
+            Console.Write($"{live}.Hak(1-100): ");
+            guessNumber = int.Parse(Console.ReadLine());
+            if (guessNumber < generatedNumber)
+            {
+                resultMessage = "Daha büyük bir sayı giriniz!";
+            }
+            else if (guessNumber > generatedNumber)
+            {
+                resultMessage = "Daha küçük bir sayı giriniz!";
+            }
+
+            if (guessNumber != generatedNumber) live++;
+
+            if (live <= liveLimit && guessNumber != generatedNumber) Console.WriteLine(resultMessage);
+
+
+            do
+            {
+
+                Console.Write($"{live}.Hak(1-100): ");
+                guessNumber = int.Parse(Console.ReadLine());
+                if (guessNumber < generatedNumber)
+                {
+                    resultMessage = "Daha büyük bir sayı giriniz!";
+                }
+                else if (guessNumber > generatedNumber)
+                {
+                    resultMessage = "Daha küçük bir sayı giriniz!";
+                }
+
+                if (guessNumber != generatedNumber) live++;
+
+                if (live <= liveLimit && guessNumber != generatedNumber) Console.WriteLine(resultMessage);
+
+
+
+
+            } while (guessNumber != generatedNumber && live <= liveLimit);
+            resultMessage = guessNumber == generatedNumber ? $" Kazandın! \n Puanın:{(liveLimit - live + 1) * 10} " : "Kaybettin!";
+            System.Console.WriteLine("Tekrar oynamak ister misin ?");
+            deneme = Console.ReadLine();
+            Console.WriteLine(resultMessage);
+            System.Console.WriteLine("Tekrar oynamak ister misin ?");
+
+        }
+        while (deneme != "h");
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
+/*
+    Sayı Tahmin Oyunu
+
+    Uyg rast gele üreteceği 1-100 arasındaki sayıyı kullanıcı tahmin ederek bulmasını sağlayacağımız kodu yazınız.
+
+    Kullanıcının tahmini üretilen sayıdan küçükse ya da büyükse buna göre aşağı yukarı yönlendirme yapsın..
+
+    kullanıcının 5 hakkı olsun.
+
+    kullanıcı doğru tahmin yaptıysa ya da tahmin hakkı dolduysa(uygun mesajı vererek) oyun sona ersin
+*/
