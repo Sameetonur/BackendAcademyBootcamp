d441f8dcaff33e76015d8f63f43d9df7aac05b8c
Sameetonur <130676651+Sameetonur@users.noreply.github.com>
2024-09-16 19:35:52 +0300

    16.09.2024

---
+++ HomeWork/WEEK6/HomeWork16092024/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.InteropServices;
+using System.Security.Principal;
+using HomeWork16092024.Animal;
+using HomeWork16092024.HesapMankinesi;
+using HomeWork16092024.TaşıtYönetimSistemi;
+
+namespace HomeWork16092024;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Lion lion1 = new Lion("aslan",12,"Yırtıcı");
+        // lion1.MakeSound();
+
+        // Bicycle bicycle1 = new Bicycle("Yılmaz","Çocuk",2024,"VAR");
+        // bicycle1.Bilgilendirme();
+        //soru6***
+        //     ScientificCalculator sayı1 = new ScientificCalculator();
+        //   System.Console.WriteLine(sayı1.Divide(20,2));
+    }
+}
