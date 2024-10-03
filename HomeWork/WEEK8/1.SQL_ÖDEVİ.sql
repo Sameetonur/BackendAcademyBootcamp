8b7a4a5613a7abae1eae22f4dc3bd7910f587316
Sameetonur <130676651+Sameetonur@users.noreply.github.com>
2024-10-03 04:37:09 +0300

    03.10.2024

--- HomeWork/WEEK8/1.SQL_ÖDEVİ.sql
+++ HomeWork/WEEK8/1.SQL_ÖDEVİ.sql
@@ -73,12 +73,127 @@ WHERE o.Freight>50
 
 SELECT
     e.EmployeeID,
+    e.FirstName +' '+ e.LastName as [Çalışan AdSoyad],
     COUNT(o.OrderID) AS [SİPARİŞ SAYISI]
 FROM Employees e
     JOIN Orders o ON o.EmployeeID=e.EmployeeID
-    JOIN OrderDetails od ON o.OrderID=od.OrderID
-GROUP BY e.EmployeeID
+GROUP BY e.EmployeeID, e.FirstName, e.LastName
 ORDER BY e.EmployeeID
+
+--11.SORGU
+-- Ürünleri kategorileriyle birlikte listeleyin.
+SELECT
+    p.ProductName,
+    c.CategoryName
+FROM Products p
+    JOIN Categories c ON P.CategoryID = c.CategoryID
+
+--12.SORGU 
+--Her bir siparişin toplam tutarını hesaplayın.
+SELECT od.OrderID,
+    SUM(od.UnitPrice*od.Quantity) AS [TOP TUTAR]
+FROM OrderDetails od
+GROUP BY od.OrderID
+
+--13.SORGU
+--En çok satılan 5 ürünü ve satış miktarlarını listeleyin.
+SELECT TOP 5
+    p.ProductID,
+    P.ProductName,
+    od.Quantity AS [Satılan Ürün Mik],
+    od.UnitPrice
+
+FROM Products p
+    JOIN OrderDetails od ON OD.ProductID = P.ProductID
+GROUP BY p.ProductID,p.ProductName,od.UnitPrice,od.Quantity
+ORDER BY [Satılan Ürün Mik] DESC
+
+--14.SORGU
+--Her bir müşterinin verdiği sipariş sayısını bulun.
+SELECT
+    c.CustomerID ,
+    COUNT(o.OrderID) AS [sipariş Sayısı]
+
+FROM Customers c
+    JOIN Orders o ON C.CustomerID = O.CustomerID
+GROUP BY c.CustomerID
+
+--15.SORGU
+--Hangi kargo şirketinin kaç sipariş taşıdığını listeleyin.
+
+SELECT
+    s.CompanyName,
+    COUNT(o.OrderID) AS [Taşıdığı Sirapiş Sayısı]
+FROM Shippers s
+    JOIN Orders o ON s.ShipperID= o.ShipVia
+GROUP BY S.CompanyName
+ORDER BY [Taşıdığı Sirapiş Sayısı] DESC
+
+--16.SORGU
+--Her bir çalışanın toplam satış tutarını hesaplayın.
+SELECT
+    e.EmployeeID,
+    e.FirstName +' '+e.LastName AS [Çalışan AdSoyad],
+    SUM(od.UnitPrice*od.Quantity*(1-od.Discount)) AS [Yapılan Sayış Tutarı]
+
+FROM Employees e
+    JOIN Orders o ON O.EmployeeID=E.EmployeeID
+    JOIN OrderDetails od ON o.OrderID = od.OrderID
+GROUP BY e.EmployeeID,e.FirstName,e.LastName
+ORDER BY [Yapılan Sayış Tutarı] DESC
+
+
+--17.SORGU
+--Her bir kategorideki ürünlerin ortalama fiyatını bulun.
+
+SELECT
+    c.CategoryName,
+    AVG(p.UnitPrice) AS [ortalama fiyatları]
+
+FROM Categories c
+    JOIN Products p ON P.CategoryID = C.CategoryID
+
+GROUP BY c.CategoryName
+
+--18.SORGU
+--En az 5 sipariş veren müşterileri ve sipariş sayılarını listeleyin.
+
+SELECT TOP 5
+    c.CustomerID,
+    COUNT(O.OrderID) AS [SİPARİŞ SAYISI]
+FROM Customers c
+    JOIN Orders o ON o.CustomerID=c.CustomerID
+GROUP BY C.CustomerID
+ORDER BY COUNT (O.OrderID) ASC
+
+--19.SORGU
+--Her bir ülke için toplam satış tutarını hesaplayın.
+SELECT
+    c.Country,
+    SUM(od.UnitPrice*od.Quantity) AS [TOP SATIŞ TUTARI]
+FROM Customers C
+    JOIN Orders o on o.CustomerID=c.CustomerID
+    JOIN OrderDetails od ON  o.OrderID = od.OrderID
+GROUP BY c.Country
+
+--20.SORGU
+--Siparişleri, sipariş tarihine göre yıllar ve aylar bazında gruplayarak toplam satış tutarlarını listeleyin. 
+
+SELECT
+    YEAR(o.OrderDate) AS [YIL],
+    MONTH(o.OrderDate) AS [AY],
+    SUM(od.UnitPrice*od.Quantity*(1-od.Discount)) AS [Satış Tutarı]
+FROM Orders o
+    JOIN OrderDetails od ON o.OrderID=od.OrderID
+GROUP BY  YEAR(o.OrderDate),MONTH(o.OrderDate)
+
+
+
+
+    
+
+
+
 
     
       
