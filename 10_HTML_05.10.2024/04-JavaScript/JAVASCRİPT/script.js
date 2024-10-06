8e4d35c82559a63ed2e3d6869aeea824ff069e6e
Sameetonur <130676651+Sameetonur@users.noreply.github.com>
2024-10-06 15:01:16 +0300

    06.10.2024

---
+++ 10_HTML_05.10.2024/04-JavaScript/JAVASCRİPT/script.js
@@ -0,0 +1,6 @@
+ document.getElementById('button-external').addEventListener('mouseover',function(){
+    alert('İşte üzerime geldin!')
+ });
+ document.getElementById('button-external').addEventListener('dblclick',function(){
+    alert('işte çift tıkladın')
+ });
