046e442aecc196358f05a40a27732863b05b2392
Sameetonur <130676651+Sameetonur@users.noreply.github.com>
2024-10-20 11:00:40 +0300

    20.10.2024

---
+++ 12_MVC_19.10.2023/Project06_SampleMVC/Controllers/MVC01RazorSyntaxController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project06_SampleMVC.Controllers
+{
+    public class MVC01RazorSyntaxController : Controller
+    {
+     
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ 12_MVC_19.10.2023/Project06_SampleMVC/Controllers/MVC02HtmlAndTagHelpersConroller.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project06_SampleMVC.Controllers
+{
+    public class MVC02HtmlAndTagHelpersConroller : Controller
+    {
+        // GET: MVC02HtmlAndTagHelpersConroller
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
