e9e123dbb7281df9a874acf9147762916c020d25
Sameetonur <130676651+Sameetonur@users.noreply.github.com>
2024-11-04 22:03:27 +0300

    04.11.2024

---
+++ DAİLY/MVC_Uyg_04.11.2024/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MVC_Uyg_04._11._2024.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
