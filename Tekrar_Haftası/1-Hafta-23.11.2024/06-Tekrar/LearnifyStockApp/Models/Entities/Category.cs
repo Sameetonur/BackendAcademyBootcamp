7b340db712aa926fb5db8c1698cfc0ea420846a5
Sameetonur <130676651+Sameetonur@users.noreply.github.com>
2024-11-23 15:00:04 +0300

    TekrarDersler_23.11.20024

---
+++ Tekrar_Haftası/1-Hafta-23.11.2024/06-Tekrar/LearnifyStockApp/Models/Entities/Category.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace LearnifyStockApp.Models.Entities;
+
+public class Category
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
+
+
